42ad511ab9c7abb925077599f8be73ea39b35663
Daniel Quinlan <daquinlan@iinet.net.au>
2022-01-25 22:27:18 +1100

    lecture 34

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>March 28th 2021</div>
+      <h2>Car Insurance</h2>
+      <div>$294.67</div>
+    </>
+  );
+}
+
+export default ExpenseItem;
