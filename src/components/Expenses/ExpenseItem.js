047c36a20f0448e1e69f0dfc12b1f68b4dc5f339
Daniel Quinlan <daquinlan@iinet.net.au>
2022-01-28 16:36:00 +1100

    added year dropdown

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,6 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -19,7 +12,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,42 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, newFilteredYear] = useState("2020");
+
+  const filterHandler = (year) => {
+    newFilteredYear(year);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.zeExpenses[0].title}
-        amount={props.zeExpenses[0].amount}
-        date={props.zeExpenses[0].date}
-      />
-      <ExpenseItem
-        title={props.zeExpenses[1].title}
-        amount={props.zeExpenses[1].amount}
-        date={props.zeExpenses[1].date}
-      />
-      <ExpenseItem
-        title={props.zeExpenses[2].title}
-        amount={props.zeExpenses[2].amount}
-        date={props.zeExpenses[2].date}
-      />
-      <ExpenseItem
-        title={props.zeExpenses[3].title}
-        amount={props.zeExpenses[3].amount}
-        date={props.zeExpenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onAddFilter={filterHandler} />
+        <ExpenseItem
+          title={props.zeExpenses[0].title}
+          amount={props.zeExpenses[0].amount}
+          date={props.zeExpenses[0].date}
+        />
+        <ExpenseItem
+          title={props.zeExpenses[1].title}
+          amount={props.zeExpenses[1].amount}
+          date={props.zeExpenses[1].date}
+        />
+        <ExpenseItem
+          title={props.zeExpenses[2].title}
+          amount={props.zeExpenses[2].amount}
+          date={props.zeExpenses[2].date}
+        />
+        <ExpenseItem
+          title={props.zeExpenses[3].title}
+          amount={props.zeExpenses[3].amount}
+          date={props.zeExpenses[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
