d9e24a7315c9417204bd958a436ff88164c24125
Daniel Quinlan <daquinlan@iinet.net.au>
2022-01-30 20:13:49 +1100

    fin expenses project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
   const [filteredYear, newFilteredYear] = useState("2020");
 
@@ -19,6 +19,7 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onAddFilter={filterHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
